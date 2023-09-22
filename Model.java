ae31884812b558403c5b2fdeb42975bf66a10242
DSkretting <david.skretting@gmail.com>
2023-09-22 10:38:49 +0200

    collision update

--- Model.java
+++ Model.java
@@ -13,7 +13,7 @@ package bouncing_balls;
 class Model {
 
 	double areaWidth, areaHeight;
-	
+	static double gravity = 9.81;
 	Ball [] balls;
 
 	Model(double width, double height) {
@@ -23,23 +23,57 @@ class Model {
 		// Initialize the model with a few balls
 		balls = new Ball[2];
 		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.1);
 	}
 
 	void step(double deltaT) {
 		// TODO this method implements one step of simulation with a step deltaT
-		for (Ball b : balls) {
+		for (int i = 0; i < balls.length; i++) {
+            Ball a = balls[i];
 			// detect collision with the border
-			if (b.x < b.radius || b.x > areaWidth - b.radius) {
-				b.vx *= -1; // change direction of ball
-			}
-			if (b.y < b.radius || b.y > areaHeight - b.radius) {
-				b.vy *= -1;
-			}
+			if (a.x < a.radius) {
+                a.x = a.radius;
+                a.vx *= -1;
+                // Check if the ball's x position has gone past the right boundary.
+            } else if (a.x > areaWidth - a.radius) {
+                a.x = areaWidth - a.radius;
+                a.vx *= -1;
+            }
+            if (a.y < a.radius) {
+                a.y = a.radius;
+                a.vy *= -1;
+            } else if (a.y > areaHeight - a.radius) {
+                a.y = areaHeight - a.radius;
+                a.vy *= -1;
+            }
 			
-			// compute new position according to the speed of the ball
-			b.x += deltaT * b.vx;
-			b.y += deltaT * b.vy;
+			a.vy-=gravity*deltaT;
+			a.x += deltaT * a.vx;
+			a.y += deltaT * a.vy;
+
+			for (int j = i + 1; j < balls.length; j++) {
+                Ball b = balls[j];
+                double dx = a.x - b.x;
+                double dy = a.y - b.y;
+                double distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < a.radius + b.radius) {
+                    
+                    double angle = Math.atan2(dy, dx);
+                    double velaBefore = a.vx * Math.cos(angle) + a.vy * Math.sin(angle);
+                    double velbBefore = b.vx * Math.cos(angle) + b.vy * Math.sin(angle);
+
+                    // Assuming  equal mass
+                    double velaAfter = velbBefore;
+                    double velbAfter = velaBefore;
+
+                    // Update velocities
+                    a.vx += (velaAfter - velaBefore) * Math.cos(angle);
+                    a.vy += (velaAfter - velaBefore) * Math.sin(angle);
+                    b.vx += (velbAfter - velbBefore) * Math.cos(angle);
+                    b.vy += (velbAfter - velbBefore) * Math.sin(angle);
+                }
+            }
 		}
 	}
 	
